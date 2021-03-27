8546a93622a3dbd54d6744887e8049c3faf4e021
Benjamin Kahl <benjaminkahl19@gmail.com>
2021-03-27 01:21:03 +0100

    sample project md conversion

--- build.py
+++ build.py
@@ -1,33 +1,54 @@
 import re
 import markdown
+import os
 
-# Read file
-content = open("input/sample_project/sample_project.txt", "r").read()
 
-# Get all tags
-object = dict()
+def parse_tags(file_path):
+    ret = dict()
 
-tag_tegex = re.compile('\$tag\s*([^\s]*)\s*([^$]*)\$')
-tags = tag_tegex.findall(content)
+    # open file
+    c = open(file_path, "r").read()
 
-for (tag, value) in tags:
-    object[tag] = value
+    # find all tags
+    tag_tegex = re.compile('\$tag\s*([^\s]*)\s*([^$]*)\$')
+    tags = tag_tegex.findall(c)
 
-# Function for reading tags
-def get_tag(tag):
+    # insert tags into ret
+    for (tag, value) in tags:
+        ret[tag] = value
+
+    return ret
+
+
+def get_tag(object, tag):
     if tag not in object:
         print("Object does not contain tag:" + tag)
         return None
     else:
         return object[tag]
 
-# Get filetype
-ft = get_tag("filetype")
 
-if ft=="project" or ft=="proj":
-    r = open(get_tag("project_description"), "r").read()
-    o = markdown.markdown(r)
+def process(file_path):
+    tags = parse_tags(file_path)
 
-    f = open("output/out.html", "a")
-    f.write(o)
-    f.close()
+    ft = get_tag(tags, "filetype")
+
+    if ft=="project" or ft=="proj":
+        r = open(get_tag(tags, "project_description"), "r").read()
+        o = markdown.markdown(r, extensions=['extra'])
+
+        # Get file
+        f = open("output/"+get_tag(tags, "project_id")+".html", "a")
+        # Delete previous contents
+        f.seek(0)
+        f.truncate()
+        # write output
+        f.write(o)
+        f.close()
+
+
+for root, dirs, files in os.walk(os.getcwd() + "/input"):
+    for file in files:
+        if(file.endswith(".cfg")):
+            p = os.path.join(root,file)
+            process(p)
