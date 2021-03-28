8c1cc43633640a21554cd057960973db6303b833
Benjamin Kahl <benjaminkahl19@gmail.com>
2021-03-28 22:06:59 +0200

    Fixed bug introduced in last commit, used <body onunload=...> to fix firefox bug instead.

--- resources/fade-in-out/fade-in-out.js
+++ resources/fade-in-out/fade-in-out.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    
     // When the client clicks a link
     $("a").click(function(event){
 
@@ -22,7 +22,8 @@ $(document).ready(function() {
                 $('.fade-out-diag').removeClass('fade-out-diag').addClass('fade-in-diag');
                 $('.fade-out').removeClass('fade-out').addClass('fade-in');
                 // We redirect the client now. Note: There is a second 1ms delay here because Firefox does some weird stuff when you hit the back-button and then click on a link
-                $('body').delay(1).queue(function(){ window.location = linkLocation; }
+                // $('body').delay(1).queue(function(){ window.location = linkLocation; });
+                window.location = linkLocation;
             });
         }
     });
